f27c6b457471599eb2546142225cdb1ce65bb677
waldekmastykarz <waldek@mastykarz.nl>
2021-05-05 12:49:14 +0200

    Minor reformatting

--- ui/newWatchParty.js
+++ ui/newWatchParty.js
@@ -1,32 +1,27 @@
 import { createNewEvent } from '../graph/newWatchParty.js';
-import { getUserProfile } from '../graph/user.js';
 
-export async function loadWatchPartyForm() {
- 
-    document.querySelector('#newWatchParty .ms-Grid').style = 'display: none';
-    document.querySelector('#newWatchParty .createButton').style = 'display: none';
-    document.querySelector('#newWatchParty .cancelButton').style = 'display: none';
-    document.querySelector('#newWatchParty .button').addEventListener('click', GetWatchPartyForm);
-    return;
-    
-    }
-export async function GetWatchPartyForm() {
-    
-    document.querySelector('#newWatchParty .button').style = 'display: none';
-    document.querySelector('#newWatchParty .ms-Grid').style = 'display: block';
-    document.querySelector('#newWatchParty .createButton').style = 'display: block';
-    document.querySelector('#newWatchParty .cancelButton').style = 'display: block';
-    document.querySelector('#newWatchParty .createButton').addEventListener('click', createNewEvent);
-    document.querySelector('#newWatchParty .cancelButton').addEventListener('click', refresh);
+export function loadWatchPartyForm() {
+  const newWatchParty = document.getElementById('newWatchParty');
+  newWatchParty
+    .querySelectorAll('.ms-Grid, .createButton, .cancelButton')
+    .forEach(e => e.style = 'display: none');
+  newWatchParty.querySelector('.button').addEventListener('click', getWatchPartyForm);
+}
 
-    return;
-    
-  }
+export function getWatchPartyForm() {
+  const newWatchParty = document.getElementById('newWatchParty');
+  newWatchParty
+    .querySelectorAll('.ms-Grid, .createButton, .cancelButton')
+    .forEach(e => e.style = 'display: block');
+  newWatchParty.querySelector('.button').style = 'display: none';
+  newWatchParty.querySelector('.createButton').addEventListener('click', createNewEvent);
+  newWatchParty.querySelector('.cancelButton').addEventListener('click', refresh);
+}
 
-export async function refresh(){
-    document.querySelector('#newWatchParty .ms-Grid').style = 'display: none';
-    document.querySelector('#newWatchParty .createButton').style = 'display: none';
-    document.querySelector('#newWatchParty .cancelButton').style = 'display: none';
-    document.querySelector('#newWatchParty .button').style = 'display: block';
-    return;
+export function refresh() {
+  const newWatchParty = document.getElementById('newWatchParty');
+  newWatchParty
+    .querySelectorAll('.ms-Grid, .createButton, .cancelButton')
+    .forEach(e => e.style = 'display: none');
+  newWatchParty.querySelector('.button').style = 'display: block';
 }
